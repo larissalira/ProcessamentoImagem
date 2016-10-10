c41be968470762f5ae7edbffe945c174abf07f69
Larissa Lira <lrsslira@gmail.com>
2016-10-10 18:49:32 -0300

    Implementação dos filtros e Operações lógicas

--- ProcessamentoImg/View/Form1.Designer.cs
+++ ProcessamentoImg/View/Form1.Designer.cs
@@ -34,7 +34,7 @@
             this.comboBox2 = new System.Windows.Forms.ComboBox();
             this.btn_aplicar = new System.Windows.Forms.Button();
             this.panel4 = new System.Windows.Forms.Panel();
-            this.pictureBox3 = new System.Windows.Forms.PictureBox();
+            this.pictureResultado = new System.Windows.Forms.PictureBox();
             this.btn_carregarImg1 = new System.Windows.Forms.Button();
             this.btn_carregarImg2 = new System.Windows.Forms.Button();
             this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
@@ -44,15 +44,25 @@
             this.pictureBox2 = new System.Windows.Forms.PictureBox();
             this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
             this.openFileDialog2 = new System.Windows.Forms.OpenFileDialog();
+            this.inputPanel1 = new System.Windows.Forms.Panel();
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.inputPanel2 = new System.Windows.Forms.Panel();
+            this.label3 = new System.Windows.Forms.Label();
+            this.textBox3 = new System.Windows.Forms.TextBox();
             this.panel1.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.panel3.SuspendLayout();
             this.panel4.SuspendLayout();
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureResultado)).BeginInit();
             this.tableLayoutPanel1.SuspendLayout();
             this.panel6.SuspendLayout();
             this.flowPanel2.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            this.inputPanel1.SuspendLayout();
+            this.inputPanel2.SuspendLayout();
             this.SuspendLayout();
             // 
             // panel1
@@ -76,6 +86,8 @@
             // 
             // panel3
             // 
+            this.panel3.Controls.Add(this.inputPanel2);
+            this.panel3.Controls.Add(this.inputPanel1);
             this.panel3.Controls.Add(this.comboBox2);
             this.panel3.Controls.Add(this.btn_aplicar);
             this.panel3.Dock = System.Windows.Forms.DockStyle.Fill;
@@ -101,39 +113,43 @@
             "Logaritmo"});
             this.comboBox2.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
             this.comboBox2.FormattingEnabled = true;
-            this.comboBox2.Location = new System.Drawing.Point(52, 41);
+            this.comboBox2.Location = new System.Drawing.Point(40, 41);
             this.comboBox2.Name = "comboBox2";
             this.comboBox2.Size = new System.Drawing.Size(149, 21);
             this.comboBox2.TabIndex = 1;
+            this.comboBox2.SelectedValueChanged += new System.EventHandler(this.comboBox2_SelectedValueChanged);
             // 
             // btn_aplicar
             // 
             this.btn_aplicar.Anchor = System.Windows.Forms.AnchorStyles.None;
-            this.btn_aplicar.Location = new System.Drawing.Point(52, 109);
+            this.btn_aplicar.Location = new System.Drawing.Point(40, 109);
             this.btn_aplicar.Name = "btn_aplicar";
             this.btn_aplicar.Size = new System.Drawing.Size(149, 23);
             this.btn_aplicar.TabIndex = 0;
             this.btn_aplicar.Text = "Aplicar";
             this.btn_aplicar.UseVisualStyleBackColor = true;
+            this.btn_aplicar.Click += new System.EventHandler(this.btn_aplicar_Click);
             // 
             // panel4
             // 
-            this.panel4.Controls.Add(this.pictureBox3);
+            this.panel4.Controls.Add(this.pictureResultado);
             this.panel4.Dock = System.Windows.Forms.DockStyle.Fill;
             this.panel4.Location = new System.Drawing.Point(791, 73);
             this.panel4.Name = "panel4";
             this.panel4.Size = new System.Drawing.Size(240, 182);
             this.panel4.TabIndex = 4;
             // 
-            // pictureBox3
+            // pictureResultado
             // 
-            this.pictureBox3.BackColor = System.Drawing.SystemColors.ScrollBar;
-            this.pictureBox3.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.pictureBox3.Location = new System.Drawing.Point(0, 0);
-            this.pictureBox3.Name = "pictureBox3";
-            this.pictureBox3.Size = new System.Drawing.Size(240, 182);
-            this.pictureBox3.TabIndex = 0;
-            this.pictureBox3.TabStop = false;
+            this.pictureResultado.AccessibleDescription = "";
+            this.pictureResultado.AccessibleName = "";
+            this.pictureResultado.BackColor = System.Drawing.SystemColors.Menu;
+            this.pictureResultado.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.pictureResultado.Location = new System.Drawing.Point(0, 0);
+            this.pictureResultado.Name = "pictureResultado";
+            this.pictureResultado.Size = new System.Drawing.Size(240, 182);
+            this.pictureResultado.TabIndex = 0;
+            this.pictureResultado.TabStop = false;
             // 
             // btn_carregarImg1
             // 
@@ -227,7 +243,7 @@
             this.pictureBox2.Dock = System.Windows.Forms.DockStyle.Bottom;
             this.pictureBox2.Location = new System.Drawing.Point(3, 3);
             this.pictureBox2.Name = "pictureBox2";
-            this.pictureBox2.Size = new System.Drawing.Size(263, 250);
+            this.pictureBox2.Size = new System.Drawing.Size(263, 260);
             this.pictureBox2.TabIndex = 0;
             this.pictureBox2.TabStop = false;
             // 
@@ -238,6 +254,78 @@
             // openFileDialog2
             // 
             this.openFileDialog2.FileName = "openFileDialog2";
+            // 
+            // inputPanel1
+            // 
+            this.inputPanel1.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.inputPanel1.Controls.Add(this.label2);
+            this.inputPanel1.Controls.Add(this.textBox2);
+            this.inputPanel1.Controls.Add(this.textBox1);
+            this.inputPanel1.Controls.Add(this.label1);
+            this.inputPanel1.Location = new System.Drawing.Point(26, 68);
+            this.inputPanel1.Name = "inputPanel1";
+            this.inputPanel1.Size = new System.Drawing.Size(177, 35);
+            this.inputPanel1.TabIndex = 2;
+            this.inputPanel1.Visible = false;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(3, 11);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(35, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "label1";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(44, 8);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(40, 20);
+            this.textBox1.TabIndex = 1;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(131, 8);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(36, 20);
+            this.textBox2.TabIndex = 2;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(90, 11);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(35, 13);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "label2";
+            // 
+            // inputPanel2
+            // 
+            this.inputPanel2.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.inputPanel2.Controls.Add(this.label3);
+            this.inputPanel2.Controls.Add(this.textBox3);
+            this.inputPanel2.Location = new System.Drawing.Point(26, 68);
+            this.inputPanel2.Name = "inputPanel2";
+            this.inputPanel2.Size = new System.Drawing.Size(177, 35);
+            this.inputPanel2.TabIndex = 4;
+            this.inputPanel2.Visible = false;
+            // 
+            // label3
+            // 
+            this.label3.Location = new System.Drawing.Point(6, 11);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(78, 13);
+            this.label3.TabIndex = 3;
+            this.label3.Text = "label3";
+            this.label3.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(89, 8);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(36, 20);
+            this.textBox3.TabIndex = 2;
             // 
             // Form1
             // 
@@ -252,11 +340,15 @@
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
             this.panel3.ResumeLayout(false);
             this.panel4.ResumeLayout(false);
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureResultado)).EndInit();
             this.tableLayoutPanel1.ResumeLayout(false);
             this.panel6.ResumeLayout(false);
             this.flowPanel2.ResumeLayout(false);
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            this.inputPanel1.ResumeLayout(false);
+            this.inputPanel1.PerformLayout();
+            this.inputPanel2.ResumeLayout(false);
+            this.inputPanel2.PerformLayout();
             this.ResumeLayout(false);
 
         }
@@ -269,7 +361,7 @@
         private System.Windows.Forms.Panel panel3;
         private System.Windows.Forms.Button btn_aplicar;
         private System.Windows.Forms.Panel panel4;
-        private System.Windows.Forms.PictureBox pictureBox3;
+        private System.Windows.Forms.PictureBox pictureResultado;
         private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
         private System.Windows.Forms.ComboBox comboBoxOpcoes;
         private System.Windows.Forms.OpenFileDialog openFileDialog1;
@@ -278,6 +370,14 @@
         private System.Windows.Forms.ComboBox comboBox2;
         private System.Windows.Forms.FlowLayoutPanel flowPanel2;
         private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.Panel inputPanel1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Panel inputPanel2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox textBox3;
     }
 }
 
--- ProcessamentoImg/View/Form1.cs
+++ ProcessamentoImg/View/Form1.cs
@@ -34,13 +34,13 @@ namespace ProcessamentoImg
                 comboBox2.Items.Add("Média");
                 comboBox2.Items.Add("Mediana");
                 comboBox2.Items.Add("Passa Alta");
-                comboBox2.Items.Add("Passa Baixa");
+                comboBox2.Items.Add("Prewitt");
                 comboBox2.Items.Add("Alto Reforço");
                 comboBox2.Items.Add("Sobel");
                 comboBox2.Items.Add("Gradiente");
                 comboBox2.Items.Add("Gradiente Cruzado");
                 comboBox2.Items.Add("Negativo");
-                comboBox2.Items.Add("Gama");
+                comboBox2.Items.Add("Gamma");
                 comboBox2.Items.Add("Logaritmo");
                 
             }
@@ -109,6 +109,217 @@ namespace ProcessamentoImg
             }
         }
 
-       
+        private void btn_aplicar_Click(object sender, EventArgs e)
+        {
+            if(_imagemCarregada == null)
+            {
+                MessageBox.Show("Por favor selecione uma imagem!");
+            }
+            if (comboBoxOpcoes.Text.Equals("Filtros"))
+            {
+                filtros();
+            }
+            else if (comboBoxOpcoes.Text.Equals("Operações Numéricas"))
+            {
+                opNumericas();
+            }
+            else if (comboBoxOpcoes.Text.Equals("Gato de Arnold"))
+            {
+                gatoArnold();
+            }
+            else if (comboBoxOpcoes.Text.Equals("Histograma"))
+            {
+                histograma();
+            }
+            else if (comboBoxOpcoes.Text.Equals("Operações Geométricas"))
+            {
+                opGeometricas();
+            }
+            else
+            {
+                MessageBox.Show("Por favor selecione uma opção!");
+            }
+        }
+
+        private void comboBox2_SelectedValueChanged(object sender, EventArgs e)
+        {
+            if (comboBox2.Text.Equals("Alto Reforço")){
+                inputPanel2.Visible = true;
+                label3.Text = "A: ";
+                MessageBox.Show("A>=1\nUtilize ',' para números decimais!");
+
+            }
+            else if (comboBox2.Text.Equals("Gamma")){
+                inputPanel2.Visible = true;
+                label3.Text = "Gamma: ";
+                MessageBox.Show("0<=Gamma<=1 \nUse ',' para números decimais!");
+            }
+            else if (comboBox2.Text.Equals("Logaritmo"))
+            {
+                inputPanel2.Visible = true;
+                label3.Text = "Constante: ";
+                MessageBox.Show("Utilize ',' para números decimais!");
+            }
+            else
+            {
+                inputPanel1.Visible = false;
+                inputPanel2.Visible = false;
+            }
+        }
+
+        private void filtros()
+        {
+            GerenciamentoFiltros gerFiltros = new GerenciamentoFiltros();
+
+
+            string op = comboBox2.Text;
+            switch (op)
+            {
+                case "Média":
+                    pictureResultado.Image = (Image)gerFiltros.FiltroMedia(_imagemCarregada).Clone();
+                    pictureResultado.Update();
+                    break;
+                case "Mediana":
+                    pictureResultado.Image = (Image)gerFiltros.FiltroMediana(_imagemCarregada).Clone();
+                    pictureResultado.Update();
+                    break;
+                case "Passa Alta":
+                    pictureResultado.Image = (Image)gerFiltros.FiltroPassaAlta(_imagemCarregada).Clone();
+                    pictureResultado.Update();
+                    break;
+                case "Prewitt":
+                    pictureResultado.Image = (Image)gerFiltros.FiltroPrewitt(_imagemCarregada).Clone();
+                    pictureResultado.Update();
+                    break;
+                case "Alto Reforço":
+                    if (textBox3.Text.Equals(""))
+                    {
+                        MessageBox.Show("Por favor preencha o valor de A");
+                    }
+                    else
+                    {
+                        double a = Convert.ToDouble(textBox3.Text);
+                        pictureResultado.Image = (Image)gerFiltros.FiltroAltoReforco(_imagemCarregada, a).Clone();
+                        pictureResultado.Update();
+                    }
+                    break;
+                case "Sobel":
+                    pictureResultado.Image = (Image)gerFiltros.FiltroSobel(_imagemCarregada).Clone();
+                    pictureResultado.Update();
+                    break;
+                case "Gradiente":
+                    pictureResultado.Image = (Image)gerFiltros.FiltroGradiente(_imagemCarregada).Clone();
+                    pictureResultado.Update();
+                    break;
+                case "Gradiente Cruzado":
+                    pictureResultado.Image = (Image)gerFiltros.FiltroGradienteCruzado(_imagemCarregada).Clone();
+                    pictureResultado.Update();
+                    break;
+                case "Negativo":
+                    pictureResultado.Image = (Image)gerFiltros.FiltroNegativo(_imagemCarregada).Clone();
+                    pictureResultado.Update();
+                    break;
+                case "Gamma":
+                    if (textBox3.Text.Equals(""))
+                    {
+                        MessageBox.Show("Por favor preencha o valor de Gamma");
+                    }
+                    else
+                    {
+                        double gamma = Convert.ToDouble(textBox3.Text);
+                        pictureResultado.Image = (Image)gerFiltros.FiltroGama(_imagemCarregada, gamma).Clone();
+                        pictureResultado.Update();
+                    }
+                    break;
+                case "Logaritmo":
+                    if (textBox3.Text.Equals(""))
+                    {
+                        MessageBox.Show("Por favor preencha o valor da Constante");
+                    }
+                    else
+                    {
+                        double c = Convert.ToDouble(textBox3.Text);
+                        pictureResultado.Image = (Image)gerFiltros.FiltroLogaritmo(_imagemCarregada, c).Clone();
+                        pictureResultado.Update();
+                    }
+                    break;
+                default:
+                    MessageBox.Show("Por favor selecione o filtro que deseja utilizar!");
+                    break;
+
+            }
+        }
+
+        private void opNumericas()
+        {
+            string op = comboBox2.Text;
+            switch (op)
+            {
+                case "Soma":
+
+                    break;
+                case "Subtração":
+
+                    break;
+                case "Multiplicação":
+
+                    break;
+                case "Divisão":
+
+                    break;
+                case "AND":
+
+                    break;
+                case "OR":
+
+                    break;
+                case "XOR":
+
+                    break;
+                default:
+                    MessageBox.Show("Por favor selecione a operação que deseja fazer!");
+                    break;
+
+
+            }
+        }
+
+        private void opGeometricas()
+        {
+            string op = comboBox2.Text;
+            switch (op)
+            {
+                case "Escala":
+
+                    break;
+                case "Rotação":
+
+                    break;
+                case "Cisalhamento":
+
+                    break;
+                case "Reflexão":
+
+                    break;
+                case "Translação":
+
+                    break;
+
+                default:
+                    MessageBox.Show("Por favor selecione a operação que deseja fazer!");
+                    break;
+
+            }
+        }
+
+        private void gatoArnold()
+        {
+
+        }
+
+        private void histograma()
+        {
+
+        }
     }
 }
